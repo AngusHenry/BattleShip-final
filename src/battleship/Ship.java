2a4916c846d49630740b513587e2170bbefb782f
angus <angus@192.168.2.14>
2019-06-19 22:37:54 -0400

    updating grid thing (has nulls)

---
+++ src/battleship/Ship.java
@@ -0,0 +1,35 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship;
+
+/**
+ *
+ * @author anhen3335
+ */
+public class Ship {
+    private int fire;
+    private int crit;
+    private int damage;
+    
+    public Ship(){
+    fire = 1;
+    crit = (int) (Math.random() * 6);
+    damage = 30000;
+}
+    public int getFire(){
+        return fire;
+    }
+    public int getCrit(){
+        crit = (int) (Math.random() * 6);
+        return crit;
+    }
+
+    int getDamage() {
+       return damage;
+    }
+}
