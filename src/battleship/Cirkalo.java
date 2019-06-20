b1852442ad1fffa3b9f2d02e9394a5e978a711ff
angus <angus@192.168.2.14>
2019-06-19 22:37:43 -0400

    updating grid thing (has nulls)

---
+++ src/battleship/Cirkalo.java
@@ -0,0 +1,38 @@
+    
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship;
+
+/**
+ *
+ * @author anhen3335
+ */
+public class Cirkalo extends Ship{
+    private int health;
+    private int segments;
+    private int damage; 
+    private String Ship;
+    
+    public Cirkalo (){
+        health = 150;
+        segments = 3;
+        damage = 25;
+        Ship = "Cirkalo";
+    }
+    public int getDamage(){
+    return damage;
+    }
+    public String getShip (){
+        return Ship;
+       }
+            public int getHealth (){
+        return health;
+       }
+        public int getSegments (){
+        return segments;
+       }
+}
